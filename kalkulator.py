5cae53b6e1bc662513918000edb1f01b35b9b869
Karolina <karolina.skrzypska@gmail.com>
2017-09-16 14:32:15 +0200

    Pierwszy

---
+++ kalkulator.py
@@ -0,0 +1,9 @@
+print('Kalkulator')
+a = input('Podaj pierwsza liczbe: ')
+b = input('Podaj drugą liczbe: ')
+a = float(a)
+b = float(b)
+average = (a + b) / 2
+print('Średnia liczba', a, 'oraz', b, 'to', average)
+
+
